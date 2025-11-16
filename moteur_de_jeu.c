75eb6118cd41a5076d8d61e16890ef0f9b92cf3c
s-a-r-a-h-ship <sarah.segard@edu.ece.fr>
2025-11-16 12:13:25 +0100

    combinaison colonne

--- moteur_de_jeu.c
+++ moteur_de_jeu.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int combinaison_ligne(int x, int y, int tab1[25][45]){
+void combinaison_ligne(int x, int y, int tab1[25][45]){
     int i, j, item_supprimé; 
     for(i=0; i<25; i++){
         if(tab1[i][j]==tab1[i][j+1] && tab1[i][j+1]==tab1[i][j+2] && tab1[i][j+2]==tab1[i][j+3] && tab1[i][j+3]==tab1[i][j+4] && tab1[i][j+4]==tab1[i][j+5]){              
@@ -12,6 +12,28 @@ int combinaison_ligne(int x, int y, int tab1[25][45]){
             }
             if(tab1[i][j]==0){
                 for(i; i>0; i--){
+                    tab1[i][j]=tab1[i-1][j];
+                }
+                if(i==0){
+                    tab1[i][j]= 1 + rand() % (5);
+                }
+            }
+        }
+    }
+}
+
+void combinaison_colonne(int x, int y, int tab1[25][45]){
+    int i, j, item_supprimé; 
+    for(j=0; j<45; j++){
+        if(tab1[i][j]==tab1[i+1][j] && tab1[i+1][j]==tab1[i+2][j] && tab1[i+2][j]==tab1[i+3][j] && tab1[i+3][j]==tab1[i+4][j] && tab1[i+4][j]==tab1[i+5][j]){              
+            item_supprimé=tab1[i][j];
+        }
+        for(i=0; i<25; i++){
+            if(tab1[i][j]==item_supprimé){
+                tab1[i][j]=0;
+            }
+            if(tab1[i][j]==0){
+                for(j; j>0; j--){
                     tab1[i][j]=tab1[i-1][j];
                 }
                 if(i==0){
