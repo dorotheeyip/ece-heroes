1f04758cf370d0cc28619876dcecd9373b5eb245
s-a-r-a-h-ship <sarah.segard@edu.ece.fr>
2025-11-19 16:04:01 +0100

    Update moteur_de_jeu.c

--- moteur_de_jeu.c
+++ moteur_de_jeu.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int main(){
+    int i, j;
+    for(i=0; i<25; i++){
+        for(j=0; j<45; j++){
+            int tab1[25][45]=1 + rand() % (5);
+        }
+    }
+}
 void combinaison_ligne(int x, int y, int tab1[25][45]){
     int i, j, item_supprime=-1; 
     for(i=0; i<25; i++){
