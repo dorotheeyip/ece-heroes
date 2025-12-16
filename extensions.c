94377b59e590cdd658df73fc9f602684b0fbc967
dorotheeyip <dorothee.yip@edu.ece.fr>
2025-12-16 15:38:48 +0100

    maj des constantes LINE COLUMN

--- extensions.c
+++ extensions.c
@@ -3,32 +3,32 @@
 #include <time.h>
 #include "extensions.h"
 
-void bombe(int plateau[N][P], int ligne, int colonne){
+void bombe(int plateau[LINE][COLUMN], int ligne, int colonne){
     for(int i=ligne-2; i<ligne+3; i++){
         for(int j=colonne-2; j<colonne+3; j++){
-            if (i>=0 && i<N && j>=0 && j<P) plateau[i][j]=0;
+            if (i>=0 && i<LINE && j>=0 && j<COLUMN) plateau[i][j]=0;
         }
     }
 }
 
-void fusee(int plateau[N][P], int ligne, int colonne, int orientation){
+void fusee(int plateau[LINE][COLUMN], int ligne, int colonne, int orientation){
     if(orientation==1){ // verticale
-        for(int i=0; i<N; i++){
+        for(int i=0; i<LINE; i++){
             plateau[i][colonne]=0;
         }
     }
     else if(orientation==2){ // horizontale
-        for(int j=0; j<P; j++){
+        for(int j=0; j<COLUMN; j++){
         plateau[ligne][j]=0;
         } 
     }
 }
 
-void joker(int plateau[N][P], int ligne, int colonne, int type){ 
+void joker(int plateau[LINE][COLUMN], int ligne, int colonne, int type){ 
     plateau[ligne][colonne]=type;
 }
 
-void placer_itembonus(int plateau[N][P], int ligne, int colonne, int typebonus, int orientation, int type){
+void placer_itembonus(int plateau[LINE][COLUMN], int ligne, int colonne, int typebonus, int orientation, int type){
     if (typebonus==1){
         bombe(plateau, ligne, colonne);
     }
@@ -40,10 +40,10 @@ void placer_itembonus(int plateau[N][P], int ligne, int colonne, int typebonus, 
     }
 }
 
-int detecter_diagonale4(int plateau[N][P], int *ligne, int *colonne, int *orientation){
-    for (int i=0; i<N-3; i++){
-        for (int j=0; j<P; j++){
-            if (j<P-3){
+int detecter_diagonale4(int plateau[LINE][COLUMN], int *ligne, int *colonne, int *orientation){
+    for (int i=0; i<LINE-3; i++){
+        for (int j=0; j<COLUMN; j++){
+            if (j<COLUMN-3){
                 if (plateau[i][j]==plateau[i+1][j+1] && plateau[i][j]==plateau[i+2][j+2] && plateau[i][j]==plateau[i+3][j+3]){
                     *ligne=i;
                     *colonne=j;
@@ -64,9 +64,9 @@ int detecter_diagonale4(int plateau[N][P], int *ligne, int *colonne, int *orient
     return 0;
 }
 
-int detecter_carre2x2(int plateau[N][P], int *ligne, int *colonne){
-    for (int i=0; i<N-1; i++){
-        for (int j=0; j<P-1; j++){
+int detecter_carre2x2(int plateau[LINE][COLUMN], int *ligne, int *colonne){
+    for (int i=0; i<LINE-1; i++){
+        for (int j=0; j<COLUMN-1; j++){
             if(plateau[i][j]==plateau[i][j+1] && plateau[i][j]==plateau[i+1][j] && plateau[i][j]==plateau[i+1][j+1]){
                 *ligne=i;
                 *colonne=j;
@@ -77,17 +77,17 @@ int detecter_carre2x2(int plateau[N][P], int *ligne, int *colonne){
     return 0;
 }
 
-int detecter_ligne7(int plateau[N][P], int *ligne, int *colonne){
-    for (int i=0; i<N; i++){
-        for(int j=0; j<P; j++){
-            if (j<P-7){
+int detecter_ligne7(int plateau[LINE][COLUMN], int *ligne, int *colonne){
+    for (int i=0; i<LINE; i++){
+        for(int j=0; j<COLUMN; j++){
+            if (j<COLUMN-7){
                 if (plateau[i][j]==plateau[i][j+1] && plateau[i][j]==plateau[i][j+2] && plateau[i][j]==plateau[i][j+3] && plateau[i][j]==plateau[i][j+4] && plateau[i][j]==plateau[i][j+5] && plateau[i][j]==plateau[i][j+6]){
                     *ligne=i;
                     *colonne=j;
                     return 1;
                 }
             }
-            if (i<N-7){
+            if (i<LINE-7){
                 if (plateau[i][j]==plateau[i+1][j] && plateau[i][j]==plateau[i+2][j] && plateau[i][j]==plateau[i+3][j] && plateau[i][j]==plateau[i+4][j] && plateau[i][j]==plateau[i+5][j] && plateau[i][j]==plateau[i+6][j]){
                     *ligne=i;
                     *colonne=j;
@@ -99,14 +99,14 @@ int detecter_ligne7(int plateau[N][P], int *ligne, int *colonne){
     return 0;
 }
 
-int detecter_figures_speciales(int plateau[N][P], int *ligne, int *colonne, int *orientation){
+int detecter_figures_speciales(int plateau[LINE][COLUMN], int *ligne, int *colonne, int *orientation){
     if (detecter_diagonale4(plateau, ligne, colonne, orientation)) return 1;
     else if (detecter_carre2x2(plateau, ligne, colonne)) return 2;
     else if (detecter_ligne7(plateau, ligne, colonne)) return 3;
     return 0;
 }
 
-void effet_diagonale4(int plateau[N][P], int ligne, int colonne, int orientation){ //ajout orientation 1=vers droite 2=vers gauche
+void effet_diagonale4(int plateau[LINE][COLUMN], int ligne, int colonne, int orientation){ //ajout orientation 1=vers droite 2=vers gauche
     int l=0 ,c=0;
     if (orientation==1){
         for(int i=0; l>=0 && c>=0; i++){
@@ -114,19 +114,19 @@ void effet_diagonale4(int plateau[N][P], int ligne, int colonne, int orientation
             c=colonne-i;
             plateau[l][c]=0;
         }
-        for(int i=0; l<N && c<P; i++){
+        for(int i=0; l<LINE && c<COLUMN; i++){
             l=ligne+i;
             c=colonne+i;
             plateau[l][c]=0;
         }
     }
     else if (orientation==2) {
-        for(int i=0; l>=0 && c<P; i++){
+        for(int i=0; l>=0 && c<COLUMN; i++){
             l=ligne-i;
             c=colonne+i;
             plateau[l][c]=0;
         }
-        for(int i=0; l<N && c>=0; i++){
+        for(int i=0; l<LINE && c>=0; i++){
             l=ligne+i;
             c=colonne-i;
             plateau[l][c]=0;
@@ -134,15 +134,15 @@ void effet_diagonale4(int plateau[N][P], int ligne, int colonne, int orientation
     }
 }
 
-void effet_carre2x2(int plateau[N][P], int ligne, int colonne){
+void effet_carre2x2(int plateau[LINE][COLUMN], int ligne, int colonne){
     int l[16]={0,0,1,1,-1,-1,-1,-1,0,1,2,2,2,2,1,0};
     int c[16]={0,1,0,1,-1,0,1,2,2,2,2,1,0,-1,-1,-1};
     for(int i=0; i<16; i++){
-        if (ligne+l[i]>=0 && ligne+l[i]<N && colonne+c[i]>=0 && colonne+c[i]<P) plateau[ligne+l[i]][colonne+c[i]]=0;
+        if (ligne+l[i]>=0 && ligne+l[i]<LINE && colonne+c[i]>=0 && colonne+c[i]<COLUMN) plateau[ligne+l[i]][colonne+c[i]]=0;
     }
 }
 
-void effet_ligne7(int plateau[N][P], int ligne, int colonne, int orientation){ 
+void effet_ligne7(int plateau[LINE][COLUMN], int ligne, int colonne, int orientation){ 
     int special=plateau[ligne][colonne]+5;
     if (orientation==1) {
         for (int i=0; i<7; i++){
@@ -158,7 +158,7 @@ void effet_ligne7(int plateau[N][P], int ligne, int colonne, int orientation){
     }
 }
 
-void effet_item_ligne7(int plateau[N][P], int ligne, int colonne){
+void effet_item_ligne7(int plateau[LINE][COLUMN], int ligne, int colonne){
     fusee(plateau, ligne, colonne, 1);
     fusee(plateau, ligne, colonne, 2);
     effet_diagonale4(plateau, ligne, colonne, 1);
